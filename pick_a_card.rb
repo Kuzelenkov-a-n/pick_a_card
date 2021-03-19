7ab20026cd667d25a1b222941c9412cdf900dd21
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-03-19 12:59:40 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,12 +1,22 @@
 puts 'Pick a Card. (c) goodprogrammer.ru'
 
-# Создадим массив с набором номиналов карт
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
-# Создадим массив с набором мастей.
-# Пики, Черви, Буби, Крести
 suits = %w[♠ ♥ ♦ ♣]
 
-# Выведем произвольную карты, выбрав по одному
-# произвольному элементу из массивов
-puts "#{values.sample}#{suits.sample}"
+cards = []
+
+values.each do |value|
+  suits.each do |suit|
+    cards << "#{value} of #{suit}"
+  end
+end
+
+cards.shuffle!
+
+puts 'How many cards do you pick?'
+number = STDIN.gets.to_i
+
+number.times do
+  puts cards.pop
+end
